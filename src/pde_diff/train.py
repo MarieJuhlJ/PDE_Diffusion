c253d9b6f6b8234d8889b4769bbda6717b9cac95
MarieJuhlJ <mariejuhljorgensen@gmail.com>
2025-09-29 10:47:44 +0200

    Add structure based on cookiecutter template

---
+++ src/pde_diff/train.py
@@ -0,0 +1,10 @@
+from pde_diff.model import Model
+from pde_diff.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
