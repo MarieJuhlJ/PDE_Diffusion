cb210d3ac95c5a84dfaec92f4390b06629ac3dfd
MarieJuhlJ <mariejuhljorgensen@gmail.com>
2025-11-05 11:02:38 +0100

    :police_car: Update tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,7 @@
-from omegaconf import OmegaConf
-from src.pde_diff.model import DiffusionModel
 
 def test_model_dimensions():
+    from omegaconf import OmegaConf
+    from src.pde_diff.model import DiffusionModel
     # Quick test to verify the registrys etc. work, no test of correctness of the model
 
     model = DiffusionModel(OmegaConf.create({
@@ -13,7 +13,6 @@ def test_model_dimensions():
             "max_epochs": 10,
             "log_every_n_steps": 10}
         },
-        "dataset": {"name": "mse"},
         "scheduler": {
             "name": "ddpm",
             "num_train_timesteps": 10,
@@ -21,15 +20,25 @@ def test_model_dimensions():
             "beta_end": 0.02
         },
         "loss": {"name": "mse"},
-        "model": {"name": "dummy"},
+        "model": {"name": "dummy",
+                  "save_best_model": True,
+                  "monitor": "val_loss",
+                  "mode": "min",
+                  "save_dir": './models',
+                  "id": None,
+                  "x0_pred": 'mean'
+        },
         "dataset":
         {
             "dims": {
                 "x": 64,
                 "y": 64,
-                "z": 3
-            }
+                "z": 3,
+                "input_dims": 26,
+                "output_dims": 6
+            },
+            "validation_metrics": ['mse']
         }
     }))
 
-    assert model.sample_loop(batch_size=2).shape == (2, int(model.dataset.dims.z), int(model.dataset.dims.x), int(model.dataset.dims.y))
+    assert model.sample_loop(batch_size=2).shape == (2, int(model.dataset.dims.output_dims), int(model.dataset.dims.x), int(model.dataset.dims.y))
