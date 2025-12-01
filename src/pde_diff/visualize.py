947baae3a8af9c2129c4b8678afbac260f1f2db2
MarieJuhlJ <mariejuhljorgensen@gmail.com>
2025-12-01 11:16:12 +0100

    :camera_flash: Visualize era5 samples with and without noise

--- src/pde_diff/visualize.py
+++ src/pde_diff/visualize.py
@@ -1,22 +1,47 @@
 from pathlib import Path
 import yaml
 
+from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 from omegaconf import OmegaConf
+import pandas as pd
 import torch
 
 from pde_diff.data.datasets import ERA5Dataset
 from pde_diff.model import DiffusionModel
 from pde_diff.utils import dict_to_namespace
 
+plt.style.use("pde_diff.custom_style")
 
 # Used for plotting samples:
 EXTENT_FULL = [0.0, 359.25, 90.0, -90.0]
 EXTENT_SUBSET = [0.0, 359.25, 69.75, 46.5]
+PLOT_TYPE = ".png"
 
-def plot_samples(model, n=4, out_dir=Path('./reports/figures')):
+VAR_NAMES = {
+    "u": "u",
+    "v": "v",
+    "t": "T",
+    "z": r"$\Phi$",
+    "pv": "q",
+}
+
+colors = ["brown", "white", "teal"]  # Transition from blue to white to red
+custom_cmap = LinearSegmentedColormap.from_list("custom_cmap", colors)
+colors = ["teal", "white", "brown"]  # Transition from blue to white to red
+custom_cmap2 = LinearSegmentedColormap.from_list("custom_cmap", colors)
+
+COLOR_BARS = {
+    "u": "RdBu",
+    "v": "RdBu",
+    "t": "coolwarm",
+    "z": custom_cmap2,
+    "pv": custom_cmap,
+}
+
+def plot_darcy_samples(model, model_id, n=4, out_dir=Path('./reports/figures')):
     save_dir = Path(out_dir) / model_id
     save_dir.mkdir(parents=True, exist_ok=True)
     samples = model.sample_loop(batch_size=n)
@@ -29,8 +54,8 @@ def plot_samples(model, n=4, out_dir=Path('./reports/figures')):
         axs[1, i].imshow(samples[i, 1], cmap='magma')
         axs[1, i].set_title(f'Sample {i+1} - P')
         axs[1, i].axis('off')
-    plt.savefig(save_dir / 'samples.png', bbox_inches='tight')
-    print(f"Saved samples to {save_dir / 'samples.png'}")
+    plt.savefig(save_dir / f'samples{PLOT_TYPE}', bbox_inches='tight')
+    print(f"Saved samples to {save_dir / f'samples{PLOT_TYPE}'}")
 
 def plot_training_metrics(model_id, out_dir=Path("./reports/figures")):
     df = pd.read_csv(Path("./logs") / model_id / "version_0" / "metrics.csv").sort_values(["epoch", "step"])
@@ -59,7 +84,7 @@ def plot_training_metrics(model_id, out_dir=Path("./reports/figures")):
             ax.set_yscale("log")
         ax.grid(True, alpha=0.3)
         ax.figure.tight_layout()
-        ax.figure.savefig(save_dir / f"{col}.png", dpi=150)
+        ax.figure.savefig(save_dir / f"{col}{PLOT_TYPE}", dpi=150)
         plt.close(ax.figure)
 
     if {"val_loss", "val_mse"}.issubset(df.columns):
@@ -73,7 +98,7 @@ def plot_training_metrics(model_id, out_dir=Path("./reports/figures")):
             ax.grid(True, alpha=0.3)
             ax.legend()
             ax.figure.tight_layout()
-            ax.figure.savefig(save_dir / "val_metrics_combined.png", dpi=150)
+            ax.figure.savefig(save_dir / f"val_metrics_combined{PLOT_TYPE}", dpi=150)
             plt.close(ax.figure)
 
 
@@ -95,7 +120,7 @@ def get_data_sample(dataset, sample_idx, variable, level=500):
     return data
 
 
-def visualize_era5_sample(data_sample, variable, level=500, big_data_sample=None,sample_idx=None):
+def visualize_era5_sample(data_sample, variable, level=500, big_data_sample=None, sample_idx=None, dir=Path("./reports/figures/samples")):
     """
     Visualize a sample from the ERA5 dataset.
 
@@ -107,19 +132,19 @@ def visualize_era5_sample(data_sample, variable, level=500, big_data_sample=None
         sample_idx (int, optional): Index of the sample (for title purposes).
     """
     # Plot the data
-    fig, ax = plt.subplots(figsize=(8,6))
+    fig, ax = plt.subplots(figsize=(6,3))
 
     if big_data_sample is not None:
         vmin = min(big_data_sample.min(), data_sample.min())
         vmax = max(big_data_sample.max(), data_sample.max())
 
         # Draw entire dataset first
-        ax.imshow(big_data_sample.T, cmap='coolwarm', extent=EXTENT_FULL, origin='lower',vmin=vmin, vmax=vmax, alpha=0.5)
+        ax.imshow(big_data_sample.T, cmap=COLOR_BARS.get(variable, 'coolwarm'), extent=EXTENT_FULL, origin='lower',vmin=vmin, vmax=vmax, alpha=0.5)
         ax.set_xlim(EXTENT_FULL[0], EXTENT_FULL[1])
         ax.set_ylim(EXTENT_FULL[3], EXTENT_FULL[2])
 
         # Draw subset on top in correct place
-        ax.imshow(data_sample.T, cmap='coolwarm', extent=EXTENT_SUBSET, origin='lower',vmin=vmin, vmax=vmax)
+        ax.imshow(data_sample.T, cmap=COLOR_BARS.get(variable, 'coolwarm'), extent=EXTENT_SUBSET, origin='lower',vmin=vmin, vmax=vmax)
         rect = patches.Rectangle(
             (EXTENT_SUBSET[0], EXTENT_SUBSET[2]),           # bottom-left corner (lon_min, lat_min)
             EXTENT_SUBSET[1]-EXTENT_SUBSET[0],              # width (lon_max - lon_min)
@@ -129,18 +154,92 @@ def visualize_era5_sample(data_sample, variable, level=500, big_data_sample=None
             facecolor='none'                           # transparent fill
         )
         ax.add_patch(rect)
-
+        fig.colorbar(ax.images[-1], ax=ax, shrink=0.4)
     else:
-        ax.imshow(data_sample.T, cmap='coolwarm', extent=EXTENT_SUBSET, origin='lower')
+        ax.imshow(data_sample.T, cmap=COLOR_BARS.get(variable, 'coolwarm'), extent=EXTENT_SUBSET, origin='lower')
+        fig.colorbar(ax.images[-1], ax=ax, shrink=0.4, location="bottom")
 
-    fig.colorbar(ax.images[-1], ax=ax, shrink=0.4, location="bottom")
-    ax.set_title(f"{variable.capitalize()} at {level} hPa")
+    ax.set_title(f"{VAR_NAMES.get(variable, variable)} at {level} hPa")
     ax.set_xlabel("Longitude")
     ax.set_ylabel("Latitude")
     plt.tight_layout()
-    plot_path = f"reports/figures/samples/era5_sample{sample_idx if sample_idx is not None else ""}_{variable}_{level}hPa"
+    plot_path = f"era5_sample{sample_idx if sample_idx is not None else ''}_{variable}_{level}hPa"
     plot_path += "_full" if big_data_sample is not None else ""
-    plt.savefig(plot_path + ".png")
+    plot_path += PLOT_TYPE
+    plot_path = dir / plot_path
+    plt.savefig(plot_path, bbox_inches='tight', pad_inches=0.05)
+    print(f"Saved visualization to {plot_path}")
+
+def visualize_era5_sample_full(big_data_sample, variable, level=500, sample_idx=None, dir=Path("./reports/figures/samples")):
+    """
+    Visualize a sample from the ERA5 dataset.
+
+    Args:
+        data_sample (np.ndarray): The data sample to visualize.
+        variable (str): Variable to visualize (e.g., 'temperature').
+        level (int): Pressure level to focus on (default: 500hPa).
+        big_data_sample (np.ndarray, optional): The entire dataset for context.
+        sample_idx (int, optional): Index of the sample (for title purposes).
+    """
+    # Plot the data
+    fig, ax = plt.subplots(figsize=(8,4))
+
+    # Draw entire data
+    ax.imshow(big_data_sample.T, cmap=COLOR_BARS.get(variable, 'coolwarm'), extent=EXTENT_FULL, origin='lower')
+    #remove all ticks and labels
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    plot_path = f"era5_sample{sample_idx if sample_idx is not None else ''}_{variable}_{level}hPa_full_only"
+    plot_path += PLOT_TYPE
+    plot_path = dir / plot_path
+    plt.savefig(plot_path, bbox_inches='tight', pad_inches=0)
+    print(f"Saved visualization to {plot_path}")
+
+def visualize_noise_schedule(scheduler, data_sample, variable, level=500, sample_idx=None, dir=Path("./reports/figures/samples"), steps= 10):
+    """
+    Visualize a sample from the ERA5 dataset.
+
+    Args:
+        data_sample (np.ndarray): The data sample to visualize.
+        variable (str): Variable to visualize (e.g., 'temperature').
+        level (int): Pressure level to focus on (default: 500hPa).
+        sample_idx (int, optional): Index of the sample (for title purposes).
+    """
+    # Plot the data
+    fig, ax = plt.subplots(figsize=(data_sample.shape[1] / 5, data_sample.shape[0] / 5))
+    ax.imshow(data_sample.T, cmap=COLOR_BARS.get(variable, 'coolwarm'), extent=EXTENT_SUBSET, origin='lower')
+    ax.set_xlim(EXTENT_SUBSET[0], EXTENT_SUBSET[1])
+    ax.set_ylim(EXTENT_SUBSET[3], EXTENT_SUBSET[2])
+    ax.axis('off')  # Remove axes
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0) #Remove margins
+
+    # Save the plot
+    plot_path = f"era5_sample{sample_idx if sample_idx is not None else ''}_{variable}_{level}hPa_noise_{0}"
+    plot_path += PLOT_TYPE
+    plot_path = dir / plot_path
+    plt.savefig(plot_path, bbox_inches='tight', pad_inches=0)
+    plt.close(fig)
+    print(f"Saved visualization to {plot_path}")
+
+    for t in range(100,scheduler.config.num_train_timesteps+1, scheduler.config.num_train_timesteps//steps):
+        noised_sample = scheduler.add_noise(torch.tensor(data_sample).unsqueeze(0).unsqueeze(0).float(), torch.randn_like(torch.tensor(data_sample).unsqueeze(0).unsqueeze(0).float()), torch.tensor([t-1]))
+        noised_sample = noised_sample.squeeze().squeeze().numpy()
+        fig, ax = plt.subplots(figsize=(noised_sample.shape[1] / 5, noised_sample.shape[0] / 5))
+        ax.imshow(noised_sample.T, cmap=COLOR_BARS.get(variable, 'coolwarm'), extent=EXTENT_SUBSET, origin='lower')
+        ax.set_xlim(EXTENT_SUBSET[0], EXTENT_SUBSET[1])
+        ax.set_ylim(EXTENT_SUBSET[3], EXTENT_SUBSET[2])
+        ax.axis('off')  # Remove axes
+        plt.subplots_adjust(left=0, right=1, top=1, bottom=0) #Remove margins
+
+        # Save the plot
+        plot_path = f"era5_sample{sample_idx if sample_idx is not None else ''}_{variable}_{level}hPa_noise_{t}"
+        plot_path += PLOT_TYPE
+        plot_path = dir / plot_path
+        plt.savefig(plot_path, bbox_inches='tight', pad_inches=0)
+        plt.close(fig)
+        print(f"Saved visualization to {plot_path}")
+
 
 if __name__ == "__main__":
     #model_path = Path('./models')
@@ -167,12 +266,25 @@ if __name__ == "__main__":
 
     era5_dataset_full = ERA5Dataset(cfg)
 
-    # Visualize a sample from the dataset
+    # Visualize all variables of a sample from the dataset
     sample_idx = 0  # Index of the sample to visualize
     variable = "t"  # Variable to visualize
     level = 500  # Pressure level in hPa
+    for variable in cfg.atmospheric_features:
+        data_sample = get_data_sample(era5_dataset, sample_idx, variable, level)
+        data_sample_full = get_data_sample(era5_dataset_full, sample_idx, variable, level)
+        visualize_era5_sample(data_sample, variable, level, big_data_sample=data_sample_full)
 
+    # Visualize the noise schedule
+    config_path = Path("configs/scheduler/ddpm.yaml")
+    cfg_scheduler = OmegaConf.load(config_path)
+    from pde_diff.utils import SchedulerRegistry
+    scheduler = SchedulerRegistry.create(cfg_scheduler)
+
+    variable = "t"
     data_sample = get_data_sample(era5_dataset, sample_idx, variable, level)
-    data_sample_full = get_data_sample(era5_dataset_full, sample_idx, variable, level)
 
-    visualize_era5_sample(data_sample, variable, level, big_data_sample=data_sample_full)
+    #visualize_noise_schedule(scheduler, data_sample, variable, level, sample_idx)
+
+    # Visualize a full sample from the dataset without the subset overlay
+    visualize_era5_sample_full(data_sample_full, variable, level)
